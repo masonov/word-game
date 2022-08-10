e96b9322b117a93295f38d9c6a4c406d7c2aaff9
Denis <denis@mukhus.com>
2022-08-10 20:51:02 +0300

    доработана проверка слов

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
-let letters = document.querySelectorAll('.letters span');
+let letters = document.querySelectorAll('.letters button');
 let wordRows = document.querySelectorAll('.words .row');
 let images = document.querySelectorAll('.images img');
 
 let wordsArr = ['aaaaaa', 'bbbbbbb', 'nnnnn'];
 
-let currentRow = 0;
+let currentRow = 0; // какая строка заполняется буквами, меняется при нажатии на изображения
 
 for (let i = 0; i < images.length; i++) {
 	images[i].onclick = function() {
-		currentRow = this.dataset.num;
+		currentRow = this.dataset.num; // добавляем к изображениям обрабочик клика, при котором меняется номер строки в которую вводим слово
 
 	}
 }
 
 for (let i = 0; i < letters.length; i++) {
 	letters[i].onclick = function() {
-		addLetter(this);
+		addLetter(this); // при нажатии на буквы добавляем букву в поле с помощью функции addLetter(). В функцию передаем обьект на который нажали
+	}
+}
+
+function changeRow(color, backColor, reset = false) {
+	let currentRowSubElems = wordRows[currentRow].querySelectorAll('div'); // находим все клетки текущей строки
+	for (let i = 0; i < currentRowSubElems.length; i++) { // перебираем все клетки и каждую окрашиваем в зеленый цвет
+		currentRowSubElems[i].style.background = backColor;
+		currentRowSubElems[i].style.color = color;
+		if (reset) {
+			currentRowSubElems[i].innerText = '';
+		}
 	}
 }
 
 function addLetter(targetElem) {
-	for (let i = 0; i < wordRows[currentRow].children.length; i++) {
-		if (wordRows[currentRow].children[i].innerText == '') {
-			wordRows[currentRow].children[i].innerText = targetElem.innerText;
-			if (i == wordRows[currentRow].children.length - 1) {
+	for (let i = 0; i < wordRows[currentRow].children.length; i++) { // перебираем все строки где вводятся слова
+
+		if (wordRows[currentRow].children[i].innerText == '') { // перебираем все клетки строки на пустое значение
+			wordRows[currentRow].children[i].innerText = targetElem.innerText;  // если значение пустое то добавляем букву в текущую клетку
+
+			if (i == wordRows[currentRow].children.length - 1) { // если цикл с перебором клеток дошел до номера последней клетки
+
 				if( wordsArr.includes(wordRows[currentRow].innerText
 					.split('\n')
 					.join("")
-					.toLowerCase()) ) {
-					
-					let currentRowSubElems = wordRows[currentRow].querySelectorAll('div');
-					for (let i = 0; i < currentRowSubElems.length; i++) {
-						currentRowSubElems[i].style.background = 'green';
-						currentRowSubElems[i].style.color = 'white';
-					}
+					.toLowerCase()) ) { // проверяем содержится ли наше собранное слово в списке верных ответов
+
+					changeRow('white', 'green'); // если слово подходит то меняем цвет клеток на зеленый
+					currentRow++; // меняем номер строки на следующий
+				} else {
+					changeRow('white', 'red'); // если слово не подходит то меняем цвет клеток на красный
+
+					setTimeout(function() {
+						alert('ответ неверный'); // и выводим окно 
+					}, 200);
+
+					setTimeout(function() {
+						changeRow('black', '', true); // ждем секунду чтобы наш код не сработал раньше открытия окна
+					}, 1000);
 				}
+
 			}
+
 			break;
 		}
 	}
